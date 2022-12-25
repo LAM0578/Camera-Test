5728b13e207dabb74920d2544ac63023f62b86fd
LAM0578 <1805096805@qq.com>
2022-12-26 01:43:44 +0800

    Add FPS Show.

--- Assets/Scripts/ApplicationInfoText.cs
+++ Assets/Scripts/ApplicationInfoText.cs
@@ -1,15 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ApplicationInfoText : MonoBehaviour
 {
     private void Awake()
-        => this
-            .gameObject
-            .GetComponent<Text>()
-            .text = "Press \"R\" to reset player position.\n" + 
+    {
+        m_Text = this.gameObject.GetComponent<Text>();
+        ShowTextHead = 
+            "Press \"R\" to reset player position.\n" +
             $"{Application.productName} By {Application.companyName} \n" +
-            $"Application Version: {Application.version} Unity Version: {Application.unityVersion}";
+            $"Application Version: {Application.version} Unity Version: {Application.unityVersion}\n";
+    }
+
+    private static string ShowTextHead;
+    private Text m_Text;
+    private int Frame;
+    private float lastInterval;
+
+    // [DllImport("mono.dll")]
+    // public static extern long mono_gc_get_used_size();
+
+    private void Update()
+    {
+        ++Frame;
+        float timeNow = Time.realtimeSinceStartup;
+        float interval = timeNow - lastInterval;
+        if (interval > 0.25f)
+        {
+            // float usedMemory = mono_gc_get_used_size();
+            // SystemInfo.systemMemorySize
+            m_Text.text = 
+                ShowTextHead + 
+                $"FPS: {(float)Frame / interval}\n";
+                // $"Memory: {usedMemory} / {SystemInfo.systemMemorySize}";
+
+            Frame = 0;
+            lastInterval = timeNow;
+        }
+# if UNITY_STANDALONE
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+        else if (Input.GetKeyDown(KeyCode.Escape) && Screen.fullScreen) Screen.fullScreen = false;
+# endif
+    }
 }
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 <b>WARNING: The project is a test demo.</b>
 # Project environment
 - Unity 2018.3.10f1 or higher
+# Update Logs
+> 2022/12/26 `0.1.1`
+- Add FPS text.
+- Add quit fullscreen Key (Escpase)
+- Add quit Key (LeftControl + Escpase)
